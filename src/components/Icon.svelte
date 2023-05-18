2b7bcd2041b990167a7ee8e796e4b52d3cb9e4ac
Angelus Trindade <angelusrt@gmail.com>
2023-05-18 15:04:55 -0300

    🤠 Botão melhorado

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -71,15 +71,10 @@
       />
     </svg>
   {:else if name === "Menu"}
-    <svg viewBox="0 30 448 440">
-      <path 
-        d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 
-        32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 
-        256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 
-        32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 
-        416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 
-        32-32H416c17.7 0 32 14.3 32 32z" 
-      />
+    <svg name="svg-menu" viewBox="0 0 100 100">
+      <rect class="top" x="0" y="10" width="100" height="10"/>
+      <rect class="middle" x="0" y="45" width="100" height="10"/>
+      <rect class="bottom" x="0" y="80" width="100" height="10"/>
     </svg>
   {/if}
 </i>
--- src/components/NavButton.svelte
+++ src/components/NavButton.svelte
@@ -8,20 +8,20 @@
   let isToggle = false
   let time: NodeJS.Timeout
   
-  let dropdownRef: HTMLElement
+  let buttonRef: HTMLElement
   
   onMount(() => {isMobile = window.innerWidth <= 600})
 
   function onToggle(isToggle: boolean) {
     if(time) clearTimeout(time)
 
-    if(isToggle && dropdownRef){
-      remove(dropdownRef, "--none")
-      time = setTimeout(() => add(dropdownRef, "--show"), 10)
+    if(isToggle && buttonRef){
+      remove(buttonRef, "--none")
+      time = setTimeout(() => add(buttonRef, "--show"), 10)
     }
-    else if(!isToggle && dropdownRef ){
-      remove(dropdownRef, "--show")
-      time = setTimeout(() => add(dropdownRef, "--none"), 300)
+    else if(!isToggle && buttonRef ){
+      remove(buttonRef, "--show")
+      time = setTimeout(() => add(buttonRef, "--none"), 300)
     }
   }
   
@@ -32,13 +32,14 @@
 
 {#if isMobile}
   <button 
+    bind:this={buttonRef}
     class="button-index" 
     aria-label="Menu" 
     on:click={() => isToggle = true} 
     on:mouseleave={() => isToggle = false}
   >
     <Icon name="Menu" isBig={false}/>
-    <div bind:this={dropdownRef} class="dropdown dropdown--none">
+    <div class="dropdown">
       {#each sections as section}
         <a class="link-normal px-2.5" href={section.href}>
           {section.text}
@@ -48,13 +49,14 @@
   </button>
 {:else}
   <button 
+    bind:this={buttonRef}
     class="button-index" 
     aria-label="Menu" 
     on:mouseenter={() => isToggle = true} 
     on:mouseleave={() => isToggle = false}
   >
     <Icon name="Menu" style="mr-0" isBig={false}/>
-    <div bind:this={dropdownRef} class="dropdown dropdown--none">
+    <div class="dropdown">
       {#each sections as section}
         <a class="link-normal px-2.5" href={section.href}>
           {section.text}
@@ -74,30 +76,70 @@
     background-color: var(--light-blue);
     border: 2px solid var(--dark-green);
     fill: var(--grey);
-    border-radius: 10px;
+    border-radius: 15px;
     width: 50px;
     height: 50px;
   }
-
-  :global(.dropdown){
+  
+  :global(.button-index){    
+    transition: transform 300ms;
+    transform: rotate(0);
+  }
+  :global(.button-index .dropdown){
     opacity: 0;
-    max-height: 0px;
     position: absolute;
     bottom: 70px;
     left: 0;
+    text-align: left;
     background-color: var(--light-blue);
     border: 2px solid var(--dark-green);
     border-radius: 20px;
     overflow: hidden;
     padding: 20px;
+    transform: rotate(0deg) scale(0.9);
     transition: all 300ms ease-in-out;
   }
-  :global(.dropdown--none){
+  :global(.button-index--none .dropdown){
     display: none;
   }
-  :global(.dropdown--show){
+  :global(.button-index--show .dropdown){
     opacity: 1;
-    max-height: 300px;
+    transform: rotate(24deg) scale(1);
+    transform-origin: bottom left;
     transition: all 300ms ease-in-out;
+  }
+
+  :global(.button-index .middle) {
+    opacity: 1;
+    transition: opacity 300ms;
+  }
+  :global(.button-index .top) {
+    transform: rotate(0deg) translateX(0px);
+    transform-origin: top left;
+    transition: transform 300ms;
+  }
+  :global(.button-index .bottom) {
+    transform: rotate(0deg)  translateX(0px);
+    transform-origin: bottom left;
+    transition: transform 300ms;
+  }
+
+  :global(.button-index--show) {
+    transform: rotate(-12deg);
+    transition: transform 300ms;
+  }
+  :global(.button-index--show .middle) {
+    opacity: 0;
+    transition: opacity 300ms;
+  }
+  :global(.button-index--show .top) {
+    transform: translateX(30px) rotate(55deg); 
+    transform-origin: top left;
+    transition: transform 300ms;
+  }
+  :global(.button-index--show .bottom) {
+    transform: translateX(30px) rotate(-55deg);
+    transform-origin: bottom left;
+    transition: transform 300ms;
   }
 </style>
